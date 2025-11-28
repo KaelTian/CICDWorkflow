a228b5e70ad1fcf28428c940f05823f58c935c27
Kael.Tian <Kael_Tian@163.com>
2025-11-28 11:17:17 +0800

    add health check

---
+++ CICDDemo/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICDDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("healthy");
+    }
+}
